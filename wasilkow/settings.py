ecfa78de1053dec7f72d6c79c77c52f43e8490ce
CReflection <pawel.jakimiuk00@gmail.com>
2022-08-26 18:39:53 +0000

    fix settings

--- wasilkow/settings.py
+++ wasilkow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#w9w-h)ng$w&1_d^uy#i+=24iw6^2v(kzzzg$9v%ky^ua$csp'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config("IP_ADDR")]
 
 
 # Application definition
