cd425b73c0cf0600f40aa4b0a4447bcbfa3d2af8
goore3 <kowalik.twoja1@gmail.com>
2022-08-28 20:33:48 +0200

    Add better views validation and show all questions

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render 
 
-from .models import Question
+from .models import Question, Answer
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.all()
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        answer = get_object_or_404(Answer, pk=request.POST['answer'])
+        if(answer.valid):
+            return HttpResponse("Jest to poprawna odpowiedź na pytanie '" +answer.answer_text + "'")
+        else:
+            return HttpResponse("Nie jest to poprawna odpowiedź na pytanie '" +answer.answer_text + "'")
+
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
@@ -20,5 +27,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-# Create your views here.
