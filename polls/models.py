0371791b467df3e10e5dd4915fab82f4a730d2ff
goore3 <kowalik.twoja1@gmail.com>
2022-08-31 19:42:55 +0200

    Add group questions and migrations

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Group(models.Model):
     name = models.CharField('Grupa', max_length=120)
-    hash = models.CharField(max_length=20)
+    hash = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return str(self.name)
--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,13 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
 
-    path('qr/', views.qr, name='qr'),
+    path('<int:question_id>/', views.detail, name='detail'),
     path('question/', views.randomQuestion, name='randomQuestion'),
+    path('quiz/<slug:group_hash>', views.groupQuestion, name='groupQuestion'),
+    
+    path('qr/', views.qr, name='qr'),
+    
     path('wyniki/', views.score, name='wyniki'),
 ]
