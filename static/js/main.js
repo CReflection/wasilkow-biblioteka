89c90ffa2e8b89c626ad011e8e34d361d3ce386a
goore3 <kowalik.twoja1@gmail.com>
2022-09-02 12:42:29 +0200

    Add unfinished index

--- static/js/main.js
+++ static/js/main.js
@@ -5,12 +5,29 @@ function msToTime(s) {
     s = (s - secs) / 60;
     var mins = s % 60;
     var hrs = (s - mins) / 60;
+    
+    if(secs < 10) {
+        secs = "0" + secs
+    }
+    if(mins < 10){
+        mins = "0" + mins
+    }
+    if(hours < 10) {
+        hours = "0" + hours
+    }
+    if(ms < 10) {
+        ms = "000" + ms
+    } else if(ms < 100) {
+        ms = "00" + ms
+    } else if(ms < 1000) {
+        ms = "0" + ms
+    }
   
     return hrs + ':' + mins + ':' + secs + '.' + ms;
   }
 
 function updateTime(){
-    console.log(start_time)
+    // console.log(start_time)
     let timeDivs = document.querySelectorAll('.view-time');
     timeDivs.forEach((div)=>{
         let timeDelta = Date.now() - Date.parse(start_time);
@@ -19,3 +36,13 @@ function updateTime(){
     requestAnimationFrame(updateTime);
 }
 requestAnimationFrame(updateTime);
+
+window.onload = function(e){
+    let div = document.querySelector('#mickiewicz-container');
+    let image = document.querySelector('#mickiewicz-info img');
+    // let height = image.height
+    console.log(image.height)
+    image.style.top = "-" + (image.height - 40) + "px";
+    image.style.right = "-" + (image.width/2 - 50) + "px";
+    div.style.marginTop = image.height + "px";
+}
